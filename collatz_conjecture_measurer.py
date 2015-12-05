785ed62d86f77f129b3d04b1f699fd3383f5bb0b
Carlos Araujo <carlossdparaujo@gmail.com>
2015-12-05 20:59:52 -0200

    Creating a class for the methods and creating new method that returns the number with the longest collatz sequence between two specified values.

--- collatz_conjecture_measurer.py
+++ collatz_conjecture_measurer.py
@@ -1,13 +1,30 @@
-def apply_collatz_conjecture(n, current_iteration = 0):
-    if n == 0 or n < 0:
-        raise ValueError('N value must be equal or greater than one')
+class CollatzConjectureMeasurer:
+    def __init__(self):
+        pass
 
-    if n == 1:
-        return current_iteration
+    def apply_collatz_conjecture(self, n, current_iteration = 0):
+        if n == 0 or n < 0:
+            raise ValueError('N value must be equal or greater than one')
 
-    current_iteration += 1
-    
-    if n%2 == 0:
-        return apply_collatz_conjecture(n/2, current_iteration)
-    else:
-        return apply_collatz_conjecture(n*3 + 1, current_iteration)
+        if n == 1:
+            return current_iteration
+
+        current_iteration += 1
+        
+        if n%2 == 0:
+            return self.apply_collatz_conjecture(n/2, current_iteration)
+        else:
+            return self.apply_collatz_conjecture(n*3 + 1, current_iteration)
+
+    def get_longest_sequence_number_between(self, n_min, n_max):
+        longest_sequence_count = -1
+        longest_sequence_number = 0 
+
+        for i in range(n_min, n_max + 1):
+            sequence_count = self.apply_collatz_conjecture(i, 0)
+
+            if sequence_count > longest_sequence_count:
+                longest_sequence_count = sequence_count
+                longest_sequence_number = i
+
+        return longest_sequence_number
--- collatz_conjecture_measurer_test.py
+++ collatz_conjecture_measurer_test.py
@@ -1,30 +1,41 @@
 import unittest
-from collatz_conjecture_measurer import apply_collatz_conjecture
+from collatz_conjecture_measurer import CollatzConjectureMeasurer
 
 class CollatzConjectureMeasurerTest(unittest.TestCase):
     def test_get_number_of_iterations_starting_on_1_should_return_0(self):
-        n = apply_collatz_conjecture(1)
+        measurer = CollatzConjectureMeasurer()
+        n = measurer.apply_collatz_conjecture(1)
         self.assertIs(n == 0, True, "Iteration number was: " + str(n))
 
     def test_get_number_of_iterations_starting_on_2_should_return_1(self):
-        n = apply_collatz_conjecture(2)
+        measurer = CollatzConjectureMeasurer()
+        n = measurer.apply_collatz_conjecture(2)
         self.assertIs(n == 1, True, "Iteration number was: " + str(n))
 
     def test_get_number_of_iterations_starting_on_3_should_return_7(self):
-        n = apply_collatz_conjecture(3)
+        measurer = CollatzConjectureMeasurer()
+        n = measurer.apply_collatz_conjecture(3)
         self.assertIs(n == 7, True, "Iteration number was: " + str(n))
 
     def test_get_number_of_iterations_starting_on_4_should_return_2(self):
-        n = apply_collatz_conjecture(4)
+        measurer = CollatzConjectureMeasurer()
+        n = measurer.apply_collatz_conjecture(4)
         self.assertIs(n == 2, True, "Iteration number was: " + str(n))
 
     def test_get_number_of_iterations_starting_on_0_should_raise_value_error_exception(self):
         with self.assertRaises(ValueError):
-            n = apply_collatz_conjecture(0)
+            measurer = CollatzConjectureMeasurer()
+            n = measurer.apply_collatz_conjecture(0)
 
     def test_get_number_of_iterations_starting_on_negative_number_should_raise_value_error_exception(self):
         with self.assertRaises(ValueError):
-            n = apply_collatz_conjecture(-1)
+            measurer = CollatzConjectureMeasurer()
+            n = measurer.apply_collatz_conjecture(-1)
+
+    def test_get_number_with_longest_iteration_sequence_between_1_and_4_should_return_3(self):
+        measurer = CollatzConjectureMeasurer()
+        n = measurer.get_longest_sequence_number_between(1, 4)
+        self.assertIs(n == 3, True, "Number with longest sequence was: " + str(n))
         
 if __name__ == '__main__':
     unittest.main()
